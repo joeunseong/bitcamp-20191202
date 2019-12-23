aad7c86523b4dff5dd3049cb18e685002c201ed2
joeunseong <euns29@naver.com>
2019-12-23 16:17:41 +0900

    class / method

--- bitcamp-java/src/main/java/com/eomcs/basic/ex07/assignment/Test01.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex07/assignment/Test01.java
@@ -4,13 +4,20 @@ public class Test01 {
   public static void main(String[] args) {
     // 배열의 값 중에서 최대 값을 출력하라.
     int[] values = {34, 4, -3, 78, 12, 22, 45, 0, -22};
-    int value = max(values);
+    int value = max(values); //메소드 호출
+    System.out.println("최대값:"+value);
 
   }
 
   static int max(int[] values) {
-    return 0;
-    // 파라미터로 배열을 받아 그 값 중에서 최대 값을 찾아 리턴한다.
+    // 파라미터로 배열을 받고 그 값 중에서 최대 값을 찾아 리턴한다.
+    int maxValue = values[0];
+    for (int i = 1; i < values.length; i++) {
+      if (values[i] > maxValue) {
+        maxValue = values[i];
+      }
+    }
+    return maxValue;
   }
 
 }
