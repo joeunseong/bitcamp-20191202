d55a3e205802107234ac9909b60bc308cec88d14
joeunseong <euns29@naver.com>
2020-01-31 11:42:25 +0900

    serialization/deserialization

--- bitcamp-project/Backup/v30/src/main/main/java/com/eomcs/lms/domain/Board.java
+++ bitcamp-project/Backup/v30/src/main/main/java/com/eomcs/lms/domain/Board.java
@@ -1,40 +1,15 @@
 package com.eomcs.lms.domain;
 
-import java.io.Serializable;
 import java.sql.Date;
 
-// 객체를 serialize 하려면 이 기능을 활성화시켜야 한다.
-// - java.io.serializable을 구현하라!
-// - serialize 데이터를 구분하기 위해 버전 번호를 명시하라.
-//
-public class Board implements Serializable {
-
-  private static final long serialVersionUID = 20200131L;
+public class Board {
+  
   private int no;
   private String title;
   private Date date;
   private int viewCount;
   private String writer;
-
-  // CSV 포맷:
-  // - 번호,제목,등록일,조회수,작성자
-  //
-  public static Board valueOf(String csv) {
-    String[] data = csv.split(",");
-
-    Board board = new Board();
-    board.setNo(Integer.parseInt(data[0]));
-    board.setTitle(data[1]);
-    board.setDate(Date.valueOf(data[2]));
-    board.setViewCount(Integer.parseInt(data[3]));
-    board.setWriter(data[4]);
-    return board;
-  }
-
-  public String toCsvString() {
-    return String.format("%d,%s,%s,%d,%s", this.getNo(), this.getTitle(), this.getDate(),
-        this.getViewCount(), this.getWriter());
-  }
+  
 
   @Override
   public int hashCode() {
@@ -46,7 +21,6 @@ public class Board implements Serializable {
     result = prime * result + ((writer == null) ? 0 : writer.hashCode());
     return result;
   }
-
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -72,45 +46,36 @@ public class Board implements Serializable {
       return false;
     return true;
   }
-
   public int getNo() {
     return no;
   }
-
   public void setNo(int no) {
     this.no = no;
   }
-
   public String getTitle() {
     return title;
   }
-
   public void setTitle(String title) {
     this.title = title;
   }
-
   public Date getDate() {
     return date;
   }
-
   public void setDate(Date date) {
     this.date = date;
   }
-
   public int getViewCount() {
     return viewCount;
   }
-
   public void setViewCount(int viewCount) {
     this.viewCount = viewCount;
   }
-
   public String getWriter() {
     return writer;
   }
-
   public void setWriter(String writer) {
     this.writer = writer;
   }
-
+  
+  
 }
