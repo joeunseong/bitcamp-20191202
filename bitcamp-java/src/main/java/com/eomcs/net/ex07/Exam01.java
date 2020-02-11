21ddbc69028d86a947a3b2523d7306ba848410b3
joeunseong <euns29@naver.com>
2020-02-11 14:24:36 +0900

    URL

--- bitcamp-java/src/main/java/com/eomcs/net/ex07/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/net/ex07/Exam01.java
@@ -6,8 +6,14 @@ import java.net.URL;
 public class Exam01 {
 
   public static void main(String[] args) throws Exception {
-    // 웹 상에서 자원의 위치를 표현하는 방법 
-    URL url = new URL("http://www.bitcamp.co.kr:8080/a/b/hello.html");
+    // 웹 상에서 자원의 위치를 표현하는 방법
+    // => [프로토콜]://서버주소:포트번호/자원의경로
+    // - 프로토콜: http, https, ftp 등
+    // - 서버주소: IP 주소(192.168.0.1), 도메인명(www.bitcamp.co.kr)
+    // - 포트번호: 80(생략할 수 있다), 8080(프록시 서버) 등
+    // - 자원의경로: /index.html, /board/list.jsp 등
+    //
+    URL url = new URL("https://bitcamp.co.kr:80/index.php?main_page=home");
 
     // URL 분석
     System.out.printf("프로토콜: %s\n", url.getProtocol());
@@ -17,9 +23,5 @@ public class Exam01 {
   }
 
 }
-
-
-
-
 
 
