282d22b682362657d890226e6e28cdaf0c3b0050
joeunseong <euns29@naver.com>
2020-02-24 12:19:19 +0900

    Application Server 구조로 변경: 서버 종료 명령 처리하기

--- bitcamp-project-server/v37_5/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-project-server/v37_5/src/main/java/com/eomcs/lms/ServerApp.java
@@ -68,6 +68,7 @@ public class ServerApp {
       listener.contextDestroyed(context);
     }
   }
+  // 옵저버 관련코드 끝!
 
   public void service() {
 
@@ -103,20 +104,20 @@ public class ServerApp {
       System.out.println("클라이언트 연결 대기중...");
 
       while (true) {
-        Socket socket = serverSocket.accept(); // 대기중
+        Socket socket = serverSocket.accept();
         System.out.println("클라이언트와 연결되었음!");
 
-        // 새 클라이언트 요청을 처리하기 전에
         executorService.submit(() -> {
           processRequest(socket);
           System.out.println("--------------------------------------");
         });
-        
+
         // 현재 '서버 멈춤' 상태라면,
         // 다음 클라이언트 요청을 받지 않고 종료한다.
         if (serverStop) {
           break;
         }
+
       }
 
     } catch (Exception e) {
@@ -128,13 +129,13 @@ public class ServerApp {
     executorService.shutdown();
     // => 스레드풀을 당장 종료시키는 것이 아니다.
     // => 스레드풀에 소속된 스레드들의 작업이 모두 끝나면
-    //    스레드 풀의 동작을 종료하라는 뜻이다.
-    // => 따라서 shutdown() 을 호출했다고 해서
-    //    모든 스레드가 즉시 작업을 멈추는 것이 아니다.
+    // 스레드풀의 동작을 종료하라는 뜻이다.
+    // => 따라서 shutdown()을 호출했다고 해서
+    // 모든 스레드가 즉시 작업을 멈추는 것이 아니다.
     // => 즉 스레드풀 종료를 예약한 다음에 바로 리턴한다.
 
     // 모든 스레드가 끝날 때까지 DB 커넥션을 종료하고 싶지 않다면,
-    // 스레드가 끝났는지 검사하며 기다려야한다.
+    // 스레드가 끝났는지 검사하며 기다려야 한다.
     while (true) {
       if (executorService.isTerminated()) {
         break;
@@ -146,6 +147,7 @@ public class ServerApp {
         e.printStackTrace();
       }
     }
+
     // 클라이언트 요청을 처리하는 스레드가 모두 종료된 후에
     // DB 커넥션을 닫도록 한다.
     notifyApplicationDestroyed();
@@ -160,7 +162,6 @@ public class ServerApp {
         Scanner in = new Scanner(socket.getInputStream());
         PrintStream out = new PrintStream(socket.getOutputStream())) {
 
-      // 클라이언트가 보낸 명령을 읽는다.
       String request = in.nextLine();
       System.out.printf("=> %s\n", request);
 
@@ -169,7 +170,6 @@ public class ServerApp {
         return;
       }
 
-      // 클라이언트의 요청을 처리할 객체를 찾는다.
       Servlet servlet = servletMap.get(request);
 
       if (servlet != null) {
@@ -179,6 +179,7 @@ public class ServerApp {
         } catch (Exception e) {
           out.println("요청 처리 중 오류 발생!");
           out.println(e.getMessage());
+
           System.out.println("클라이언트 요청 처리 중 오류 발생:");
           e.printStackTrace();
         }
