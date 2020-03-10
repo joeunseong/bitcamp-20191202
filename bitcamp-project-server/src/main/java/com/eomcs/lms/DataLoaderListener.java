a06d20a552f757485a39c833bbfd811da20eb35b
joeunseong <euns29@naver.com>
2020-03-10 20:26:33 +0900

    MyBatis SQL 맵퍼 프레임워크를 사용하여 JDBC 코드를 대체하기

--- bitcamp-project-server/src/main/java/com/eomcs/lms/DataLoaderListener.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/DataLoaderListener.java
@@ -1,6 +1,10 @@
 package com.eomcs.lms;
 
+import java.io.InputStream;
 import java.util.Map;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.eomcs.lms.context.ApplicationContextListener;
 import com.eomcs.lms.dao.mariadb.BoardDaoImpl;
 import com.eomcs.lms.dao.mariadb.LessonDaoImpl;
@@ -28,12 +32,17 @@ public class DataLoaderListener implements ApplicationContextListener {
       DataSource dataSource = new DataSource(jdbcUrl, username, password);
       context.put("dataSource", dataSource);
 
+      // MyBatis 객체 준비
+      InputStream inputStream =
+          Resources.getResourceAsStream("com/eomcs/lms/conf/mybatis-config.xml");
+      SqlSessionFactory SqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
       // 이 메소드를 호출한 쪽(App)에서 DAO 객체를 사용할 수 있도록 Map 객체에 담아둔다.
-      context.put("boardDao", new BoardDaoImpl(dataSource));
-      context.put("lessonDao", new LessonDaoImpl(dataSource));
-      context.put("memberDao", new MemberDaoImpl(dataSource));
-      context.put("photoBoardDao", new PhotoBoardDaoImpl(dataSource));
-      context.put("photoFileDao", new PhotoFileDaoImpl(dataSource));
+      context.put("boardDao", new BoardDaoImpl(SqlSessionFactory));
+      context.put("lessonDao", new LessonDaoImpl(SqlSessionFactory));
+      context.put("memberDao", new MemberDaoImpl(dataSource, SqlSessionFactory));
+      context.put("photoBoardDao", new PhotoBoardDaoImpl(SqlSessionFactory));
+      context.put("photoFileDao", new PhotoFileDaoImpl(SqlSessionFactory));
 
       // 트랜잭션 관리자 준비
       PlatformTransactionManager txManager = new PlatformTransactionManager(dataSource);
