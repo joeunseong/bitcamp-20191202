0dc35f136200f4406539a83aa9c79177cc96574f
joeunseong <euns29@naver.com>
2020-03-16 09:44:05 +0900

    Mybatis를 이용하여 DAO 구현체 자동 생성하기

--- bitcamp-project-server/v45_2/src/main/java/com/eomcs/lms/DataLoaderListener.java
+++ bitcamp-project-server/v45_2/src/main/java/com/eomcs/lms/DataLoaderListener.java
@@ -29,12 +29,12 @@ public class DataLoaderListener implements ApplicationContextListener {
 
     try {
       // Mybatis 객체 준비
-      InputStream inputStream =
-          Resources.getResourceAsStream("com/eomcs/lms/conf/mybatis-config.xml");
+      InputStream inputStream = Resources.getResourceAsStream(//
+          "com/eomcs/lms/conf/mybatis-config.xml");
 
-      // 트랜잭션 제어를 위해 오리지널 객체를 프록시 객체 담아 사용한다.
-      SqlSessionFactory sqlSessionFactory =
-          new SqlSessionFactoryProxy(new SqlSessionFactoryBuilder().build(inputStream));
+      // 트랜잭션 제어를 위해 오리지널 객체를 프록시 객체에 담아 사용한다.
+      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryProxy(//
+          new SqlSessionFactoryBuilder().build(inputStream));
       context.put("sqlSessionFactory", sqlSessionFactory);
 
       // DAO 프록시 객체를 생성해 줄 Factory를 준비
@@ -48,11 +48,12 @@ public class DataLoaderListener implements ApplicationContextListener {
       PhotoFileDao photoFileDao = daoFactory.createDao(PhotoFileDao.class);
 
       // 트랜잭션 관리자 준비
-      PlatformTransactionManager txManager = new PlatformTransactionManager(sqlSessionFactory);
+      PlatformTransactionManager txManager = new PlatformTransactionManager(//
+          sqlSessionFactory);
 
       // 서블릿에서 사용할 서비스 객체를 준비한다.
       context.put("lessonService", new LessonServiceImpl(lessonDao));
-      context.put("photoBoardService",
+      context.put("photoBoardService", //
           new PhotoBoardServiceImpl(txManager, photoBoardDao, photoFileDao));
       context.put("boardService", new BoardServiceImpl2(sqlSessionFactory));
       context.put("memberService", new MemberServiceImpl(memberDao));
