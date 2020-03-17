99755541179ce4542db2de414ae6a879a12e03b4
joeunseong <euns29@naver.com>
2020-03-17 18:16:48 +0900

    인터페이스 대신 애노테이션으로 메서드 구분하기

--- bitcamp-project-server/src/main/java/com/eomcs/util/ApplicationContext.java
+++ bitcamp-project-server/src/main/java/com/eomcs/util/ApplicationContext.java
@@ -1,6 +1,7 @@
 package com.eomcs.util;
 
 import java.io.File;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
@@ -42,6 +43,32 @@ public class ApplicationContext {
             clazz.getName());
       }
     }
+  }
+
+  // ?만 있으면 어떤 타입이든 다 받는다는 의미라 extends 하면 그걸 상속 받은 애만 올 수 있음
+  public String[] getBeanNamesForAnnotation(Class<? extends Annotation> annotationType) {
+    // 특정 애노테이션이 붙은 객체를 찾아 보자.
+
+    // => 객체 이름을 저장할 목록을 준비한다.
+    ArrayList<String> beanNames = new ArrayList<>();
+
+    // => 객체풀에서 전체 객체의 이름을 꺼낸다.
+    Set<String> beanNameSet = objPool.keySet();
+    for (String beanName : beanNameSet) {
+
+      // 객체풀에서 이름을 이용하여 객체를 한 개 꺼낸다.
+      Object obj = objPool.get(beanName);
+
+      // 해당 객체에 파라미터로 지정한 애노테이션이 붙었는지 알아낸다.
+      if (obj.getClass().getAnnotation(annotationType) != null) {
+        beanNames.add(beanName);
+      }
+    }
+
+    // ArrayList에서 문자열을 배열로 받는다.
+    String[] names = new String[beanNames.size()];
+    beanNames.toArray(names);
+    return names;
   }
 
   public void printBeans() {
--- bitcamp-project-server/src/main/java/com/eomcs/util/RequestMapping.java
+++ bitcamp-project-server/src/main/java/com/eomcs/util/RequestMapping.java
@@ -1,13 +1,19 @@
 package com.eomcs.util;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 // 실행 중에 메소드에 붙은 애노테이션 정보를 추출해야 한다.
 @Retention(RetentionPolicy.RUNTIME)
+
+// 메소드에만 이 애노테이션을 붙일 수 있도록 사용 범위를 제한한다.
+@Target({ElementType.METHOD})
+
 public @interface RequestMapping {
   // 메소드와 명령어를 연결하기 위해,
   // 명렁어를 저장할 프로퍼티를 정의한다.
-
+  String value();
 
 }
