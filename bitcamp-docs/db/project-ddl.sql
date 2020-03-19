5568b093b1e633d57aab37bb75d792ab7abfd5fa
joeunseong <euns29@naver.com>
2020-03-19 14:15:53 +0900

    spring

--- bitcamp-docs/db/project-ddl.sql
+++ bitcamp-docs/db/project-ddl.sql
@@ -14,7 +14,7 @@ drop table if exists lms_photo;
 drop table if exists lms_photo_file;
 
 -- 수업 테이블 생성
-create table lms_lesson (
+create table info (
   lesson_id int not null auto_increment primary key comment '수업 데이터 식별 번호', 
   sdt datetime not null comment '수업 시작일',
   edt datetime not null comment '수업 종료일',
@@ -49,24 +49,24 @@ create table lms_board (
 ) comment '게시물';
 
 -- 사진 게시물 테이블 생성
-create table lms_photo (
+create table movie_photo (
   photo_id int not null auto_increment primary key comment '사진 게시물 식별 번호',
-  lesson_id int not null comment '수업 번호',
+  info_id int not null comment '영화 정보 번호',
   titl varchar(255) not null comment '제목',
   cdt datetime default now() comment '생성일',
   vw_cnt int default 0 comment '조회수',
-  -- lesson_id에 저장되는 값은 lms_lesson 테이블의 lesson_id 값으로 제한하는 조건을 추가한다.
-  constraint fk_photo_to_lesson foreign key (lesson_id)
-    references lms_lesson (lesson_id)
+  -- info_id에 저장되는 값은 movie_info 테이블의 info_id 값으로 제한하는 조건을 추가한다.
+  constraint fk_photo_to_info foreign key (info_id)
+    references movie_info (info_id)
 ) comment '사진게시물';
 
 -- 사진 게시물에 첨부하는 사진 파일 테이블 생성
-create table lms_photo_file (
+create table movie_photo_file (
   photo_file_id int not null auto_increment primary key comment '사진 파일 식별 번호',
   photo_id int not null,
   file_path varchar(255) not null,
   constraint fk_photo_file_to_photo foreign key (photo_id)
-    references lms_photo (photo_id)
+    references movie_photo (photo_id)
 ) comment '사진 게시물 첨부파일 테이블'; 
 
 
