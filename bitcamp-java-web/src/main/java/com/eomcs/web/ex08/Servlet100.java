6c9c0d0f2262c4389189998dae1e47b0c1346992
joeunseong <euns29@naver.com>
2020-04-07 16:17:10 +0900

    servlet

--- bitcamp-java-web/src/main/java/com/eomcs/web/ex08/Servlet100.java
+++ bitcamp-java-web/src/main/java/com/eomcs/web/ex08/Servlet100.java
@@ -1,5 +1,5 @@
-// 포워딩(forwarding) - 서블릿 실행을 위임하기
-package bitcamp.ex07;
+// 리프래시 또는 리다이렉트 최종 요청 페이지
+package com.eomcs.web.ex08;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,19 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ex07/s3")
+@WebServlet("/ex08/s100")
 @SuppressWarnings("serial")
-public class Servlet03 extends HttpServlet {
-  
+public class Servlet100 extends HttpServlet {
+
   @Override
-  protected void service(
-      HttpServletRequest request, 
-      HttpServletResponse response)
+  protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    
+
+    // 이 서블릿은 직접 실행할 용도로 만든 것이 아니다.
+    // 리프래시와 리다이렉트를 연습할 용도로 만들었다.
+    //
     response.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = response.getWriter();
-    out.println("해당 연산을 수행할 수 없습니다.");
+
+    out.println("반가워요 - /ex08/s100");
   }
 }
 
